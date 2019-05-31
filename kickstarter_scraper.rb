3925847e618962b9f8ee766f7a41f366d6ac9e7a
elijahmm17 <elijahmm17@gmail.com>
2019-05-31 00:01:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ def create_project_hash
   results = []
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+  kickstarter.css('li.project.grid_4')
 end
 
 create_project_hash
