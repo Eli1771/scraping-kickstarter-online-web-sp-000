b82d5eb04652f749daf2ef262a474aa1c72e2b09
elijahmm17 <elijahmm17@gmail.com>
2019-05-31 00:08:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = kickstarter.css('li.project.grid_4')
   projects.each do |p|
-    :projects[p.css('.bbcard')]
+    :projects[p.css('.bbcard_name strong')] = {}
   end 
 end
 
